63054bf1b244a1f5c542ba2153201b1e4fe72f38
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-09 07:27:12 +0100

    Created and Added contents to Content Component

---
+++ src/Content.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div className="container-fluid">
+      <div className='content container'>
+        <section>
+          <h5>Hello</h5>
+          <h2>I'm <strong>Yusuf Azeez</strong></h2>
+          <h4>Developer / Programmer / Engineer</h4>
+          <button>Hire Me</button>
+          <button>Portfolio</button>
+          <div>
+            <img src="{}" alt="author-img" />
+          </div>
+        </section>
+        <section>
+          <img src="{}" alt="author-img" />
+          <h5>WHO AM I?</h5>
+          <h2>I'm <strong>Yusuf Azeez</strong></h2>
+          <h2>A Developer / Programmer / Engineer</h2>
+          <p>
+            Hi there! I'm very passionate about web development.I build professional
+            and bespoke websites. I am looking for a web developer position in a
+            reputed software company that can help me to achieve my goal.
+          </p>
+          <button>View CV</button>
+        </section>
+        <section>
+          <h5>MY RESUME</h5>
+          <h2>I Would Love To Make Your Ideas Real</h2>
+          <p>
+            I can give your business a new creative start right away! Hire me
+            for your Awesome Project!
+          </p>
+          <button>View Resume</button>
+        </section>
+        <section>
+          <h5>GET IN TOUCH</h5>
+          <h2>Let's Start A Project! Hire Me.</h2>
+          <button>Hire Me</button>
+          <button>Get In Touch</button>
+        </section>
+    </div>
+    </div>
+  )
+}
