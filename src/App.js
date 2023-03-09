9d2ec4a613fb11de0b2c2b25c3f64e77f38babb8
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-09 07:28:19 +0100

    Added: Content component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Content from "./Content";
 import Navbar from "./Navbar";
 
 function App() {
-  return <div className="App">
-    <nav>
+  return (
+    <div className="App">
       <Navbar />
-    </nav>
-  </div>;
+      <main>
+        <Content />
+      </main>
+    </div>
+  );
 }
 
 export default App;
