f5cb11e06aa704955c61901a1548eab675ccf547
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-10 16:12:38 +0100

    Created and Added: Contents to Footer Component

---
+++ src/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <p>YussaMoney © Copyright 2023. Developed by YussaMoney</p>
+      <div>
+        <ul>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
