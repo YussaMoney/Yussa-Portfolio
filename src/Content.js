db72e6b7e990077363d5509f700aeb12ce5fe591
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-10 15:45:17 +0100

    Modified: Content component; - Styled and Added images to Content component

--- src/Content.js
+++ src/Content.js
@@ -1,55 +1,95 @@
-import React from 'react'
+import React from "react";
+import authorPic from "./images/author-pic.jpg";
+import authorImg from "./images/me4 (2).jpg";
 
 export default function Content() {
   return (
     <div className="container-fluid px-0">
-      <div className='content'>
-        <section className='bg-secondary-subtle py-5'>
-          <div className="container">
-            <h5 className='text-secondary bolder'><strong>HELLO</strong></h5>
-            <h1 className='py-3'>I'm <strong>Yusuf Azeez</strong></h1>
-            <h4 className='text-footer pb-3 text-scondary'>Developer / Programmer / Engineer</h4>
-            <button className='py-3 px-5 btn btn-primary border-0 fs-5'>Hire Me</button>
-            <button className='py-3 px-5 m-3 btn btn-outline-primary fs-5'>Portfolio</button>
-          </div>
-          <div>
-            <img src="{}" alt="author-img" />
+      <div className="content">
+        <section className="bg-secondary-subtle py-5">
+          <div className="container d-flex profile">
+            <div className="hello">
+              <h5 className="text-secondary bolder">
+                <strong>HELLO</strong>
+              </h5>
+              <h1 className="py-3">
+                I'm <strong>Yusuf Azeez</strong>
+              </h1>
+              <h4 className="text-footer pb-3 text-secondary">
+                Developer / Programmer / Engineer
+              </h4>
+              <button className="py-3 px-5 btn btn-primary border-0 fs-5">
+                Hire Me
+              </button>
+              <button className="py-3 px-5 m-3 btn btn-outline-primary fs-5">
+                Portfolio
+              </button>
+            </div>
+            <div className="authorImg">
+              <img src={authorPic} className="w-100" alt="author-img" />
+            </div>
           </div>
         </section>
-        <section className='py-5'>
+        <section className="py-5">
+          <div className="container d-flex profile">
+            <div className="authorImg">
+              <img src={authorImg} className="w-100" alt="author-img" />
+            </div>
+            <div className="who">
+              <h5 className="text-secondary">
+                <strong>WHO AM I?</strong>
+              </h5>
+              <h2 className="pt-3">
+                I'm <strong>Yusuf Azeez</strong>
+              </h2>
+              <h2 className="pb-3">A Developer / Programmer / Engineer</h2>
+              <h5 className="text-secondary pb-5 lh-base">
+                Hi there! I'm a Knowledgeable Front End Developer adept at
+                creating successful websites that meet customer needs.
+                Specializing in collaborating with customers to gather
+                requirements, produce plans and improve designs for usability and
+                functionality. Technically proficient and analytical problem
+                solver with calm and focused demeanor.
+              </h5>
+              <button className="py-3 px-5 btn btn-primary border-0 fs-5">
+                View CV
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="bg-secondary-subtle py-5">
           <div className="container">
-            <img src="{}" alt="author-img" />
-            <h5 className='text-secondary'><strong>WHO AM I?</strong></h5>
-            <h2 className='py-3'>I'm <strong>Yusuf Azeez</strong></h2>
-            <h2 className='pb-3'>A Developer / Programmer / Engineer</h2>
-            <h5 className='text-secondary pb-5'>
-              Hi there! I'm very passionate about web development.I build professional
-              and bespoke websites. I am looking for a web developer position in a
-              reputed software company that can help me to achieve my goal.
+            <h5 className="text-secondary pb-3">
+              <strong>MY RESUME</strong>
             </h5>
-            <button className='py-3 px-5 btn btn-primary border-0 fs-5'>View CV</button>
-          </div>
-        </section>
-        <section className='bg-secondary-subtle py-5'>
-          <div className="container">
-            <h5 className='text-secondary pb-3'><strong>MY RESUME</strong></h5>
-            <h2 className='pb-3'>I Would Love To Make Your Ideas Real</h2>
-            <h5 className='text-secondary pb-5'>
-              I can give your business a new creative start right away! Hire me
-              for your Awesome Project!
+            <h2 className="pb-3 fw-bolder">
+              I Would Love To Make Your Ideas Real
+            </h2>
+            <h5 className="text-secondary pb-5 lh-base">
+              I'm very passionate about web development. <br />
+              I can give your business a new creative start right away! <br />{" "}
+              Hire me for your Awesome Project!
             </h5>
-            <button className='py-3 px-5 btn btn-primary border-0 fs-5'>View Resume</button>
+            <button className="py-3 px-5 btn btn-primary border-0 fs-5">
+              View Resume
+            </button>
           </div>
         </section>
-        <section className='py-5 text-center'>
+        <section className="py-5 text-center">
           <div className="container">
-            <h5 className='text-secondary'><strong>GET IN TOUCH</strong></h5>
-            <h2>Let's Start A Project! Hire Me.</h2>
-            <button className='py-3 px-5 btn btn-primary  border-0 fs-5'>Hire Me</button>
-            <button className='py-3 px-5 m-3 my-5 btn btn-outline-primary fs-5'>Get In Touch</button>
+            <h5 className="text-secondary">
+              <strong>GET IN TOUCH</strong>
+            </h5>
+            <h2 className=" fw-bolder pb-4">Let's Start A Project! Hire Me.</h2>
+            <button className="py-3 px-5 btn btn-primary  border-0 fs-5">
+              Hire Me
+            </button>
+            <button className="py-3 px-5 m-3 btn btn-outline-primary fs-5">
+              Get In Touch
+            </button>
           </div>
         </section>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
