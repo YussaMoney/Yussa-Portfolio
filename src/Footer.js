400232b10e13dd3ee418cbd6408e388f5b396e79
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-11 12:57:21 +0100

    Modified: Footer component contents; - Styled and Added social icon

--- src/Footer.js
+++ src/Footer.js
@@ -1,24 +1,39 @@
 import React from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 
 export default function Footer() {
   return (
-    <div className='footer'>
-      <p>YussaMoney © Copyright 2023. Developed by YussaMoney</p>
-      <div>
-        <ul>
-          <li>
-            <a href=""></a>
-          </li>
-          <li>
-            <a href=""></a>
-          </li>
-          <li>
-            <a href=""></a>
-          </li>
-          <li>
-            <a href=""></a>
-          </li>
-        </ul>
+    <div className='footer border-top'>
+      <div className="container d-flex justify-content-between py-5">
+        <h5 className='text-secondary'>YussaMoney © Copyright 2023. Developed by YussaMoney</h5>
+        <div>
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.linkedin.com/in/yussamoney">
+                <LinkedInIcon />
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://wa.me/+2348078773063">
+                <WhatsAppIcon />
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://github.com/YussaMoney">
+                <GitHubIcon />
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-secondary" href="https://www.twitter.com/YussaMoney">
+                <TwitterIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
