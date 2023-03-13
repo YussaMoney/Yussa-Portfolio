b4d520e8d7002311a4f0da2e104e0550e888b9ff
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-13 22:45:27 +0100

    Modified: Exprience component (or page); - Added: A new section with contents, - Fixed: Margin top error.

--- src/Experience.js
+++ src/Experience.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
-
 
 export default function Experience() {
   return (
-    <div className='experience'>
+    <div className="experience margin-top">
       <section className="text-center mb-3 bg-secondary-subtle">
         <div className="container py-5">
           <h1 className="heading">Experience</h1>
@@ -20,6 +19,19 @@ export default function Experience() {
           </div>
         </div>
       </section>
+      <section className="py-5">
+        <div className="container text-center">
+          <h5 className="text-secondary">
+            <strong>MY WORK EXPERIENCE</strong>
+          </h5>
+          <h1 className="pb-5">
+            Kindly check out my resume below for my work experience
+          </h1>
+          <button className="py-3 px-5 btn btn-primary border-0 fs-5">
+            View Resume
+          </button>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
