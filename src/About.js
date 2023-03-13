6b46777932294e8016a97f8a4740ab0041c80b8b
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-13 06:54:31 +0100

    Modified: About component; - Added: social icons and my skills

--- src/About.js
+++ src/About.js
@@ -1,6 +1,10 @@
 import React from "react";
 import authorImg from "./images/me4 (2).jpg";
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 export default function About() {
   return (
@@ -9,10 +13,13 @@ export default function About() {
         <div className="container py-5">
           <h1 className="heading">About Me</h1>
           <div className="d-flex justify-content-center">
-            <a className="text-decoration-none active text-primary fs-5" href="/">
+            <a
+              className="text-decoration-none active text-primary fs-5"
+              href="/"
+            >
               Home
             </a>
-            <ArrowRightIcon className="text-secondary fs-2"/>
+            <ArrowRightIcon className="text-secondary fs-2" />
             <p className="text-secondary fs-5"> About Me</p>
           </div>
         </div>
@@ -38,6 +45,110 @@ export default function About() {
             <button className="py-3 px-5 mt-1 btn btn-primary border-0 fs-5">
               View CV
             </button>
+          </div>
+        </div>
+      </section>
+      <section className="bg-secondary-subtle py-5">
+        <h1 className="text-center pb-4">
+          Inspired and Motivated by the Desire to achieve Success
+        </h1>
+        <div className="container d-flex profile">
+          <div className="info">
+            <h5 className="text-secondary">
+              <strong>Name:</strong> Yusuf Azeez
+            </h5>
+            <h5 className="text-secondary">
+              <strong>From:</strong> Ibadan, Oyo
+            </h5>
+            <h5 className="text-secondary">
+              <strong>Age:</strong> 23
+            </h5>
+            <h5 className="text-secondary">
+              <strong>Email:</strong>{" "}
+              <span className="text-secondary">
+                <a
+                  className="text-decoration-none"
+                  href="mailto:highshow74@gmail.com"
+                >
+                  highshow74@gmail.com
+                </a>
+              </span>
+            </h5>
+            <ul class="nav">
+              <li class="nav-item">
+                <a
+                  class="nav-link text-secondary"
+                  href="https://www.linkedin.com/in/yussamoney"
+                >
+                  <LinkedInIcon />
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link text-secondary"
+                  href="https://wa.me/+2348078773063"
+                >
+                  <WhatsAppIcon />
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link text-secondary"
+                  href="https://github.com/YussaMoney"
+                >
+                  <GitHubIcon />
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link text-secondary"
+                  href="https://www.twitter.com/YussaMoney"
+                >
+                  <TwitterIcon />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="skills">
+            <h1 className="px-5">MY SKILLS</h1>
+            <div className="text-secondary d-grid">
+              <div className="row">
+                <h5 className="col">
+                  <strong>HTML:</strong>
+                </h5>
+                <h5 className="col-2">EXPERT</h5>
+              </div>
+              <div className="row">
+                <h5 className="col">
+                  <strong>CSS3:</strong>
+                </h5>
+                <h5 className="col-2">EXPERT</h5>
+              </div>
+              <div className="row">
+                <h5 className="col">
+                  <strong>BOOTSTRAP:</strong>
+                </h5>
+                <h5 className="col-2">EXPERT</h5>
+              </div>
+              <div className="row">
+                <h5 className="col">
+                  <strong>JAVASCRIPT:</strong>
+                </h5>
+                <h5 className="col-2">EXPERT</h5>
+              </div>
+              <div className="row">
+                <h5 className="col">
+                  <strong>REACTJS:</strong>
+                </h5>
+                <h5 className="col-2">EXPERT</h5>
+              </div>
+              <div className="row">
+                <h5 className="col">
+                  <strong>GIT/GITHUB:</strong>
+                </h5>
+                <h5 className="col-2">EXPERT</h5>
+              </div>
+            </div>
           </div>
         </div>
       </section>
