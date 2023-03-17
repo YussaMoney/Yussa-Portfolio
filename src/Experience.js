33b5f1674548099fd7c61f5de98594ad998a5d1d
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-17 04:22:39 +0100

    Modified: Experience component(or page); - Added: more sections and rewrite some text/code

--- src/Experience.js
+++ src/Experience.js
@@ -36,21 +36,6 @@ export default function Experience() {
           </button>
         </div>
       </section>
-      <section className="bg-secondary-subtle mt-5">
-        <div className="d-flex justify-content-between">
-          <img className="w-35" src={laptop} alt="laptop" />
-          <div className="py-5 my-auto">
-            <h1 className="text-center">Let's Talk</h1>
-            <a class="whatsapp-link" href="https://wa.me/+2348078773063">
-              <button className="py-3 px-5 mt-1 btn btn-primary border-0 fs-5">
-                <WhatsAppIcon className="fs-1" /> +
-                <span className="fs-5">2348078773063</span>
-              </button>
-            </a>
-          </div>
-          <img className="w-35" src={desktop} alt="desktop" />
-        </div>
-      </section>
       <section className="py-5">
         <div className="container">
           <h5 className="text-secondary text-center">
@@ -78,85 +63,100 @@ export default function Experience() {
             </a>
             <a
               className="w-30 text-decoration-none bg-secondary-subtle border rounded p-4 hover"
-              href="/"
+              href="https://yussamoney.github.io/Library"
             >
               <div class="col">
                 <div className="d-flex justify-content-between">
                   <DesignServicesIcon className="text-primary fs-1" />
                   <h1 className="heading text-secondary">02</h1>
                 </div>
-                <h1 className="pb-2 title w-50">Linktree</h1>
+                <h1 className="pb-2 title w-50">Library</h1>
                 <h4 className="text-secondary">
-                  A customizable webpage that houses all the important links
-                  that you want to share with your audience.
+                  A webpage / app where someone can add or store new book(s) and
+                  can also remove or delete book(s).
                 </h4>
               </div>
             </a>
             <a
               className="w-30 text-decoration-none bg-secondary-subtle border rounded p-4 hover"
-              href="/"
+              href="https://yussamoney.github.io/Weather-App/"
             >
               <div class="col">
                 <div className="d-flex justify-content-between">
                   <DesignServicesIcon className="text-primary fs-1" />
                   <h1 className="heading text-secondary">03</h1>
                 </div>
-                <h1 className="pb-2 title w-50">Linktree</h1>
+                <h1 className="pb-2 title w-75">Weather App</h1>
                 <h4 className="text-secondary">
-                  A customizable webpage that houses all the important links
-                  that you want to share with your audience.
+                  An app for forecasting current weather of your choice location
+                  with random beautiful image of the input location.
                 </h4>
               </div>
             </a>
             <a
               className="w-30 text-decoration-none bg-secondary-subtle border rounded p-4 hover"
-              href="/"
+              href="https://yussamoney.github.io/Simple_HTML_Calculator"
             >
               <div class="col">
                 <div className="d-flex justify-content-between">
                   <DesignServicesIcon className="text-primary fs-1" />
                   <h1 className="heading text-secondary">04</h1>
                 </div>
-                <h1 className="pb-2 title w-50">Linktree</h1>
+                <h1 className="pb-2 title">Simple HTML Calculator</h1>
                 <h4 className="text-secondary">
-                  A customizable webpage that houses all the important links
-                  that you want to share with your audience.
+                  A form-made app with only the four basic arithmetic operators
+                  (addition, subtraction, multiplication, and division).
                 </h4>
               </div>
             </a>
             <a
               className="w-30 text-decoration-none bg-secondary-subtle border rounded p-4 hover"
-              href="/"
+              href="https://yussamoney.github.io/Tic_Tac_Toe-Game/"
             >
               <div class="col">
                 <div className="d-flex justify-content-between">
                   <DesignServicesIcon className="text-primary fs-1" />
                   <h1 className="heading text-secondary">05</h1>
                 </div>
-                <h1 className="pb-2 title w-50">Linktree</h1>
+                <h1 className="pb-2 title w-75">Tic Tac Toe</h1>
                 <h4 className="text-secondary">
-                  A customizable webpage that houses all the important links
-                  that you want to share with your audience.
+                  An app / game engaged by two players(X and O) where winning is
+                  by 3 matched row or column and/or diagonal.
                 </h4>
               </div>
             </a>
             <a
               className="w-30 text-decoration-none bg-secondary-subtle border rounded p-4 hover"
-              href="/"
+              href="https://yussamoney.github.io/My_Portfolio/"
             >
               <div class="col">
                 <div className="d-flex justify-content-between">
                   <DesignServicesIcon className="text-primary fs-1" />
                   <h1 className="heading text-secondary">06</h1>
                 </div>
-                <h1 className="pb-2 title w-50">Linktree</h1>
+                <h1 className="pb-2 title w-50">Portfolio</h1>
                 <h4 className="text-secondary">
-                  A customizable webpage that houses all the important links
-                  that you want to share with your audience.
+                  A customizable webpage that showcase one's talents or creative
+                  works to potential employers or clients.
                 </h4>
               </div>
             </a>
           </div>
+        </div>
+      </section>
+      <section className="bg-secondary-subtle mt-5">
+        <div className="d-flex justify-content-between">
+          <img className="w-35" src={laptop} alt="laptop" />
+          <div className="py-5 my-auto">
+            <h1 className="text-center">Let's Talk</h1>
+            <a class="whatsapp-link" href="https://wa.me/+2348078773063">
+              <button className="py-3 px-5 mt-1 btn btn-primary border-0 fs-5">
+                <WhatsAppIcon className="fs-1" /> +
+                <span className="fs-5">2348078773063</span>
+              </button>
+            </a>
+          </div>
+          <img className="w-35" src={desktop} alt="desktop" />
         </div>
       </section>
     </div>
