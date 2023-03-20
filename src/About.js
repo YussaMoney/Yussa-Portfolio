be4a6a7706be5f9e86b3eff7e89cceffd67cf7b4
Yusuf Azeez <yabdulazeez679@stu.ui.edu.ng>
2023-03-20 08:09:58 +0100

    Added: Media query to make the About page/component responsive

--- src/About.js
+++ src/About.js
@@ -34,7 +34,7 @@ export default function About() {
             <img src={authorImg} className="w-100" alt="author-img" />
           </div>
           <div className="who">
-            <h5 className="text-secondary">
+            <h5 className="text-secondary img-footer">
               <strong>HELLO, I AM A WEB DEVELOPER</strong>
             </h5>
             <h5 className="text-secondary py-4 lh-base">
@@ -114,7 +114,7 @@ export default function About() {
             </ul>
           </div>
           <div className="skills">
-            <h1 className="px-5">MY SKILLS</h1>
+            <h1 className="px-5 my-skill">MY SKILLS</h1>
             <div className="text-secondary d-grid w-75">
               <div className="row">
                 <h5 className="col">
@@ -162,8 +162,8 @@ export default function About() {
             <strong>EDUCATION</strong>
           </h5>
           <h1 className="text-center pb-5">My Education</h1>
-          <div className="d-flex justify-content-between">
-            <div className="p-5 bg-secondary-subtle border rounded who hover">
+          <div className="d-flex justify-content-between eduList">
+            <div className="p-5 mb-3 bg-secondary-subtle border rounded who hover">
               <SchoolIcon className="text-primary fs-1 mb-3"/>
               <h2 className="pb-2">Self Taught</h2>
               <h5 className="text-secondary fw-bolder">
@@ -177,7 +177,7 @@ export default function About() {
               </h5>
               <h5 className="text-secondary lh-lg fw-bolder">2021 - Present <br />Still and Always Learning</h5>
             </div>
-            <div className="py-4 px-5 bg-secondary-subtle border rounded who hover">
+            <div className="py-4 px-5 mb-3 bg-secondary-subtle border rounded who hover">
               <PeopleIcon className="text-primary fs-1 mb-3"/>
               <h2 className="pb-2">Also Taught</h2>
               <h5 className="text-secondary fw-bolder">By
@@ -212,7 +212,7 @@ export default function About() {
       <section className="bg-secondary-subtle mt-5">
         <div className="d-flex justify-content-between">
           <img className="w-35" src={laptop} alt="laptop" />
-          <div className="py-5 my-auto">
+          <div className="py-5 my-auto talk">
             <h1 className="text-center">Let's Talk</h1>
             <a class="whatsapp-link" href="https://wa.me/+2348078773063">
               <button className="py-3 px-5 mt-1 btn btn-primary border-0 fs-5">
